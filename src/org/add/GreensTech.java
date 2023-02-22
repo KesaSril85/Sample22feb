d78e816e00fc25e007aa21c4b7462a26e3b56256
KesaSril85 <kesava.bala@gmail.com>
2023-02-22 21:49:50 +0530

    newcode

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,33 @@
+package org.add;
+
+
+public class GreensTech {
+	
+		
+		private void greensOmr() {
+			System.out.println("Green technology is located in OMR");
+		}
+		
+		private void greensAdayar() {
+			System.out.println("Green technology is located in Adayar");
+		}
+		private void greensTambaram() {
+			System.out.println("Green technology is located in Tambaram");
+		}
+		private void greensVelacherry() {
+			System.out.println("Green technology is located in Velacherry");
+		}
+		private void greensAnnaNagar() {
+			System.out.println("Green technology is located in Annanagar");
+		}
+
+		public static void main(String[]args) {
+			GreensTech eb= new GreensTech();
+		eb.greensOmr();
+		eb.greensAdayar();
+		eb.greensTambaram();
+		eb.greensVelacherry();
+		eb.greensAnnaNagar();
+		
+	}
+}
